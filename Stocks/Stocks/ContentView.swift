09ae98ab60aee2d34db6767f2ccf9ef8febf36a0
Amir Mostafavi <amireza.mostafavi@yahoo.com>
2020-01-27 19:29:20 -0600

    Initial commit

---
+++ Stocks/Stocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stocks
+//
+//  Created by Amir Mostafavi on 1/27/20.
+//  Copyright © 2020 amir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
